5c9e49b3a6d810af93f8a27df38e4bcc6809d93d
Stephan Fitzpatrick <knowsuchagency@gmail.com>
2017-02-24 02:08:17 -0800

    updated shell script to push to master instead of gh-pages

--- publish_toghpages.sh
+++ publish_toghpages.sh
@@ -16,8 +16,8 @@ mkdir public
 git worktree prune
 rm -rf .git/worktrees/public/
 
-echo "Checking out gh-pages branch into public"
-git worktree add -B gh-pages public upstream/gh-pages
+echo "Checking out master branch into public"
+git worktree add -B master public upstream/master
 
 echo "Removing existing files"
 rm -rf public/*
@@ -25,5 +25,5 @@ rm -rf public/*
 echo "Generating site"
 hugo
 
-echo "Updating gh-pages branch"
-cd public && git add --all && git commit -m "Publishing to gh-pages (publish.sh)"
+echo "Updating master branch"
+cd public && git add --all && git commit -m "Publishing to master (publish.sh)"
